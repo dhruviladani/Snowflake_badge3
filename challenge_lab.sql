54b3eb23a699e59c4df879c2f5899d5d5e16fb09
Dhruvi Ladani <110341143+dhruviladani@users.noreply.github.com>
2025-01-03 14:46:53 +0530

    Create challenge_lab.sql

---
+++ challenge_lab.sql
@@ -0,0 +1,29 @@
+select * from orders;
+
+alter table fruit_options add column SEARCH_ON varchar;
+
+update fruit_options
+set search_on = FRUIT_NAME ;
+
+select * from fruit_options;
+
+update fruit_options 
+set search_on = 'Dragonfruit'
+where FRUIT_NAME = 'Dragon Fruit';
+
+
+select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
+   SELECT 'DABW008' as step 
+   ,( select sum(hash_ing) from
+      (select hash(ingredients) as hash_ing
+       from smoothies.public.orders
+       where order_ts is not null 
+       and name_on_order is not null 
+       and (name_on_order = 'Kevin' and order_filled = FALSE and hash_ing = 7976616299844859825) 
+       or (name_on_order ='Divya' and order_filled = TRUE and hash_ing = -6112358379204300652)
+       or (name_on_order ='Xi' and order_filled = TRUE and hash_ing = 1016924841131818535))
+     ) as actual 
+   , 2881182761772377708 as expected 
+   ,'Followed challenge lab directions' as description
+); 
+
